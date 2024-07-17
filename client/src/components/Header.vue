3665d38453d6f7585d88681b03557e6b4d848f9c
mkmark53 <mkmark53@gmail.com>
2024-07-17 14:18:06 +0000

    checking in

---
+++ client/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <nav>
+            <div class="logo">
+                <h1>
+                    <a href="/">LearnlyEcommerce</a>
+                </h1>
+            </div>
+            <ul class="">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/account">account</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
